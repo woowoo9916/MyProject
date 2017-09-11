80bd73e2c7022f10d06475cf2c29f19c007908e5
woowoo9916 <woowoo9916@naver.com>
2017-09-11 11:16:17 +0900

    update

---
+++ teamProject.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	
+	string name1 = "20122969 이동환";
+	string name2 = "20101803 박다성";
+	string name3 = "20122987 정우진";
+	string name4 = "20123025 정재원";
+	string name5 = "20123028 허재준";
+	
+	cout<<"저희 6조의 구성원은 " << name1 <<", "<< name2 << ", "<< name3 << ", "<< name4 <<  ", " << name5 <<" 이렇게 총 5명 입니다";
+	
+	
+}
